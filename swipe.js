69c084763c5fc7eebf30c886246319060f91a6bb
jojosstudio <juliensperling79@gmail.com>
2024-10-23 11:49:34 +0200

    bilder hinzugefügt

--- swipe.js
+++ swipe.js
@@ -57,6 +57,23 @@ const urls = [
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-dAGKgGuVAUwOFkhnciijyPXr52LKtx759A&s',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQphEfPRHV6ZGhquCbGYmHheDO0KiFyMlB6ZQ&s',
   'https://cdn-img1.pneus-online.com/produit/pneu-auto/250/FIRESTONE_WINTERHAWK_3_S.jpg',
+  'https://m.media-amazon.com/images/I/718yxonHN8L.jpg',
+  'https://fanshop.mahle.com/media/image/67/48/b9/MAHLE_Bluetooth_Speaker_2.jpg',
+  'https://m.media-amazon.com/images/I/81loAU8Kc9L.jpg',
+  'https://gamingoo.de/wp-content/uploads/2018/07/My-new-creation.jpg',
+  'https://static.qustommize.com/img/catalog/products/85-2-de-usb-tradition-qustommize9.jpg',
+  'https://m.media-amazon.com/images/I/713CGSv7PIL.jpg',
+  'https://gzhls.at/i/38/42/3293842-n0.webp',
+  'https://www.smartlights.de/wp-content/uploads/Philips-Hue-App-Kerzen-Kaminfeuer-Szene-Zubehoer.jpg',
+  'https://m.media-amazon.com/images/I/81mQQQWLyyL.jpg',
+  'https://image.smythstoys.com/original/mobile/175181.jpg',
+  'https://abcduostorage.blob.core.windows.net/products-medium-v2/nelson-paw-patrol-grosse-helden-5985D9882.webp',
+  'https://www.webwandtattoo.com/de/img/foma434_2-jpg/folder/products-detalle-muestras-grandes/fototapeten-paw-patrol---der-kinofilm.jpg',
+  'https://static.markenkoffer.de/out/pictures/master/product/1/Heys-Nickelodeon-Paw-Patrol---2-Rollen-Trolley-46-cm_Paw-Patrol_16361-6045-00_1.jpg',
+  'https://images.foris.shop/producten/large/nickelodeon_paw_patrol_verstelbare_twee_in_een_skate_1025851_1708079323.jpg',
+  'https://cdn02.plentymarkets.com/mgt7h3rmbzvp/item/images/143302/full/143302-Lacasitos-Schokolinsen-mit-Paw-Patrol-Figur-20-Rollen-je-20g_1.jpg',
+  'https://cdn2.familie.de/6f/61/a7/0c2c93e4150406e1682b18f0ea_ZmMgN2YwMDZmYjJmZmZmIDEwMjQgNTEyAzYxMGNlNTAxMjhl.jpeg',
+  'https://image.smythstoys.com/zoom/9038203_1.jpg'
 ];
 
 // variables
@@ -65,7 +82,7 @@ let cardCount = 0;
 // functions
 function appendNewCard() {
   const card = new Card({
-    imageUrl: urls[cardCount % 15],
+    imageUrl: urls[cardCount % 68],
     onDismiss: appendNewCard,
     onLike: () => {
       like.style.animationPlayState = 'running';
